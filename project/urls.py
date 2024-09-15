963632d50be5ea82e03c9a326e7080fb533c556a
Ahme460 <ahmeoon1234@gmail.com>
2024-09-15 22:12:02 +0300

    up

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView , TokenBlacklistView  
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
+
 schema_view = get_schema_view(
    openapi.Info(
       title="API Documentation",
@@ -47,3 +52,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
