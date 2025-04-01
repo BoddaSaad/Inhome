346a3b2b9bb771e39c18f207eab449591709d3aa
BoddaSaad <abdos1166@gmail.com>
2025-04-02 01:00:17 +0200

    Fixed Email Sending

--- project/settings.py
+++ project/settings.py
@@ -211,5 +211,5 @@ CRONJOBS = [
 FIREBASE_CREDENTIALS_PATH = os.path.join(BASE_DIR, 'config', 'firebase_cred.json')
 
 LOGIN_REDIRECT_URL = '/'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
